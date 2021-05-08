476da5f171c1914b79d7a194f7740c4674fe9eaf
unknown <jinalgangani1@gmail.com>
2021-05-08 12:48:40 +0530

    LCM-HCF

---
+++ q4/q4/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace q4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButtonLCM = new System.Windows.Forms.RadioButton();
+            this.radioButtonHCF = new System.Windows.Forms.RadioButton();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // radioButtonLCM
+            // 
+            this.radioButtonLCM.AutoSize = true;
+            this.radioButtonLCM.Location = new System.Drawing.Point(320, 55);
+            this.radioButtonLCM.Name = "radioButtonLCM";
+            this.radioButtonLCM.Size = new System.Drawing.Size(57, 21);
+            this.radioButtonLCM.TabIndex = 2;
+            this.radioButtonLCM.TabStop = true;
+            this.radioButtonLCM.Text = "LCM";
+            this.radioButtonLCM.UseVisualStyleBackColor = true;
+            this.radioButtonLCM.CheckedChanged += new System.EventHandler(this.radioButtonLCM_CheckedChanged);
+            // 
+            // radioButtonHCF
+            // 
+            this.radioButtonHCF.AutoSize = true;
+            this.radioButtonHCF.Location = new System.Drawing.Point(320, 115);
+            this.radioButtonHCF.Name = "radioButtonHCF";
+            this.radioButtonHCF.Size = new System.Drawing.Size(56, 21);
+            this.radioButtonHCF.TabIndex = 3;
+            this.radioButtonHCF.TabStop = true;
+            this.radioButtonHCF.Text = "HCF";
+            this.radioButtonHCF.UseVisualStyleBackColor = true;
+            this.radioButtonHCF.CheckedChanged += new System.EventHandler(this.radioButtonHCF_CheckedChanged);
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(158, 189);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(133, 17);
+            this.labelMessage.TabIndex = 4;
+            this.labelMessage.Text = "Result Display Here";
+            this.labelMessage.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 395);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.radioButtonHCF);
+            this.Controls.Add(this.radioButtonLCM);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButtonLCM;
+        private System.Windows.Forms.RadioButton radioButtonHCF;
+        private System.Windows.Forms.Label labelMessage;
+    }
+}
+
---
+++ q4/q4/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace q4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if(radioButtonLCM.Checked)
+            {
+
+            }
+        }
+
+        private void radioButtonLCM_CheckedChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                labelMessage.Text = "Enter Number";
+            }
+            else
+            {
+                int num1 = Int32.Parse(textBox1.Text);
+                int num2 = Int32.Parse(textBox2.Text);
+                int j, hcf = 0;
+                j = (num1 > num2) ? num1 : num2;
+                for (int i = 1; i <= j; i++)
+                {
+                    if (num1 % i == 0 && num2 % i == 0)
+                    {
+                        hcf = i;
+                    }
+                }
+                labelMessage.Text = "HCF Of " + num1.ToString() + " And " + num2.ToString() + " Is: " + hcf.ToString();
+            }
+
+        }
+
+        private void radioButtonHCF_CheckedChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox1.Text == "")
+            {
+                labelMessage.Text = "Enter Number";
+            }
+            else
+            {
+                int num1 = Int32.Parse(textBox1.Text);
+                int num2 = Int32.Parse(textBox2.Text);
+                int max, lcm;
+                max = (num1 > num2) ? num1 : num2;
+                for (int i = max; ; i += max)
+                {
+                    if (i % num1 == 0 && i % num2 == 0)
+                    {
+                        lcm = i;
+                        break;
+                    }
+                }
+                labelMessage.Text = "LCM Of " + num1.ToString() + " And " + num2.ToString() + " Is: " + lcm.ToString();
+            }
+
+        }
+    }
+}
