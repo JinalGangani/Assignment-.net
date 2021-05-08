064c75c75a721e6e7189f220254c8cb5529d7f81
unknown <jinalgangani1@gmail.com>
2021-05-08 12:44:16 +0530

    text in upper case

---
+++ q2/q2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace q2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            textBox1.CharacterCasing = CharacterCasing.Upper;
+        }
+
+        private void btnsubmit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+    }
+}
